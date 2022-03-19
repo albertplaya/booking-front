dc5f63f563907a153b2044a48ec627593eab999a
Albert Playa <albertplayavidal@gmail.com>
2022-03-19 17:40:29 +0100

    Improved partner activity cards

---
+++ src/components/partnerLanding/ActivityCard.vue
@@ -0,0 +1,92 @@
+<template>
+  <q-card
+    class="m-2 rounded-md"
+    flat
+    bordered
+    style="width: 334px; height: fit-content"
+  >
+    <div
+      class="cursor-pointer"
+      @click="goToActivityEventList(activity.activity_id.value)"
+    >
+      <q-img
+        v-if="activity.image_id"
+        :src="filesUrl(activity.image_id.value)"
+        fit="cover"
+        style="max-width: 334px; height: 334px"
+      />
+      <DefaultImage v-else :activity="activity" />
+    </div>
+
+    <q-card-section horizontal>
+      <div @click="goToActivityEventList(activity.activity_id.value)">
+        <q-card-section class="q-pt-xs cursor-pointer">
+          <div class="text-h5 q-mt-sm q-mb-xs">{{ activity.title }}</div>
+        </q-card-section>
+      </div>
+      <q-space />
+      <q-btn
+        class="m-4"
+        color="grey"
+        round
+        flat
+        dense
+        :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+        @click="expanded = !expanded"
+      />
+    </q-card-section>
+
+    <q-slide-transition>
+      <div v-show="expanded">
+        <q-separator />
+        <q-card-section class="text-subitle2">
+          {{ activity.description }}
+        </q-card-section>
+      </div>
+    </q-slide-transition>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, PropType } from "vue";
+import { Activity } from "@/types/Activity";
+import DefaultImage from "@/components/activity/DefaultImage.vue";
+import router from "@/router";
+
+export default defineComponent({
+  components: { DefaultImage },
+  props: {
+    activity: {
+      type: Object as PropType<Activity>,
+      required: true,
+    },
+  },
+  setup() {
+    const expanded = ref<Boolean>(false);
+
+    const filesUrl = (id: string) =>
+      `${import.meta.env.VITE_API_FILES}/booking/${id}.jpg`;
+
+    const goToActivityEventList = async (activityId: string) => {
+      return router.push({
+        name: "activity-events-list",
+        params: { activityId: activityId },
+      });
+    };
+
+    return { filesUrl, expanded, goToActivityEventList };
+  },
+});
+</script>
+
+<style scoped>
+.description {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3; /* number of lines to show */
+  line-clamp: 2;
+  -webkit-box-orient: vertical;
+  min-height: 60px;
+}
+</style>
--- src/views/partnerLanding/ActivityCards.vue
+++ src/views/partnerLanding/ActivityCards.vue
@@ -1,39 +1,13 @@
 <template>
   <q-page class="flex justify-center bg-gray-50" padding>
-    <div style="">
+    <div style="max-width: 895px">
       <AddWhenEmptyList
         v-if="!activities.length"
         label="Add new activity"
         to="activity-add"
       />
       <div v-else class="flex row-auto justify-around mt-4">
-        <q-card
-          class="m-2 rounded-md"
-          flat
-          bordered
-          v-for="activity in activities"
-          :key="activity.activity_id"
-          style="width: 334px"
-        >
-          <router-link
-            :to="{
-              name: 'activity-events-list',
-              params: { activityId: activity.activity_id.value },
-            }"
-          >
-            <q-img
-              v-if="activity.image_id"
-              :src="filesUrl(activity.image_id.value)"
-              fit="cover"
-              style="max-width: 334px; height: 334px"
-            >
-              <div class="absolute-bottom text-subtitle2 text-center">
-                {{ activity.title }}
-              </div>
-            </q-img>
-            <DefaultImage v-else :activity="activity" />
-          </router-link>
-        </q-card>
+        <ActivityCard v-for="activity in activities" :activity="activity" />
       </div>
     </div>
   </q-page>
@@ -44,11 +18,11 @@ import { defineComponent, ref } from "vue";
 import { useActivity } from "@/use/Activity";
 import { Activity } from "@/types/Activity";
 import AddWhenEmptyList from "@/components/button/AddWhenEmptyList.vue";
-import DefaultImage from "@/components/activity/DefaultImage.vue";
+import ActivityCard from "@/components/partnerLanding/ActivityCard.vue";
 import router from "@/router";
 
 export default defineComponent({
-  components: { AddWhenEmptyList, DefaultImage },
+  components: { AddWhenEmptyList, ActivityCard },
   props: {
     partnerId: {
       type: String,
@@ -63,18 +37,14 @@ export default defineComponent({
     const activities = ref<Activity[]>([]);
 
     const listActivities = async () => {
-      console.log(props.partnerId);
       list(props.partnerId)
         .then((result) => (activities.value = result))
         .catch(() => {
           return router.push({ name: "not-found" });
         });
     };
-    const filesUrl = (id: string) =>
-      `${import.meta.env.VITE_API_FILES}/booking/${id}.jpg`;
-    return { activities, filesUrl, listActivities };
+
+    return { activities, listActivities };
   },
 });
 </script>
-
-<style scoped></style>
