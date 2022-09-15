ee65f8ed83c8a3c61ae9cfc70aa64b42da111d7b
Albert Playà Vidal <albertplayavidal@gmail.com>
2022-09-15 22:44:23 +0200

    Google loader for login and register buttons

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -21,6 +21,28 @@
           >
             <div class="flex justify-center">
               <svg
+                v-if="googleLoading"
+                class="animate-spin -ml-1 mr-3 h-5 w-5 text-teal"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              <svg
+                v-else
                 class="w-5 mr-3"
                 xmlns="http://www.w3.org/2000/svg"
                 xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -154,6 +176,7 @@ const email = ref<string>("");
 const password = ref<string>("");
 const isPwd = ref<boolean>(true);
 const errorMessage = ref();
+const googleLoading = ref(false);
 
 const router = useRouter();
 const { registerPartner } = useAuth();
@@ -177,6 +200,7 @@ const registerWithEmailAndPassword = () => {
 };
 
 const registerWithGoogle = () => {
+  googleLoading.value = true;
   const provider = new GoogleAuthProvider();
   signInWithPopup(getAuth(), provider)
     .then(async (data) => {
